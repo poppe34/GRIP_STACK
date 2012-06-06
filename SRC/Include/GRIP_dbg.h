9c63c015e4cfcb073851ee7c8864f3545a7f8ba7
mpoppe@bledsoeandassoc.com <mpoppe@bledsoeandassoc.com>
2012-06-05 22:38:33 -0500

    Minor updates

--- SRC/Include/GRIP_dbg.h
+++ SRC/Include/GRIP_dbg.h
@@ -4,7 +4,7 @@
  *  Created on: Jun 5, 2012
  *      Author: mpoppe
  *
- *      NOTE: I used most of the code from the DBG in LWIP.  It is flexable and very useful
+ *      NOTE: I used most of the code from the DBG in GRIP.  It is flexible and very useful
  */
 
 #ifndef GRIP_DBG_H_
@@ -12,19 +12,26 @@
 
 #define SYS_DBG_DISPLAY_FUNC(message)
 
-#define GRIP_DEBUG_ON		1
-
+#define GRIP_DBG_LEVEL_ALL     0x00
+#define GRIP_DBG_LEVEL_OFF     GRIP_DBG_LEVEL_ALL /* compatibility define only */
 #define GRIP_DBG_LEVEL_WARNING 0x01 /* bad checksums, dropped packets, ... */
 #define GRIP_DBG_LEVEL_SERIOUS 0x02 /* memory allocation failures, ... */
-#define GRIP_DBG_LEVEL_SEVERE  0x04
-#define GRIP_DBG_LEVEL_ALL     0x00
-#define GRIP_DBG_LEVEL_OFF     0xff /* compatibility define only */
-#define GRIP_DBG_LEVEL		   GRIP_DBG_LEVEL_ALL
+#define GRIP_DBG_LEVEL_SEVERE  0x03
+#define GRIP_DBG_MASK_LEVEL    0x03
 
+/** flag for GRIP_DEBUGF to enable that debug message */
+#define GRIP_DBG_ON            0x80U
+/** flag for GRIP_DEBUGF to disable that debug message */
+#define GRIP_DBG_OFF           0x00U
 
-#ifndef GRIP_DBG_HALT
-#define GRIP_DBG_HALT 0
-#endif
+/** flag for GRIP_DEBUGF indicating a tracing message (to follow program flow) */
+#define GRIP_DBG_TRACE         0x40U
+/** flag for GRIP_DEBUGF indicating a state debug message (to follow module states) */
+#define GRIP_DBG_STATE         0x20U
+/** flag for GRIP_DEBUGF indicating newly added code, not thoroughly tested yet */
+#define GRIP_DBG_FRESH         0x10U
+/** flag for GRIP_DEBUGF to halt after printing this debug message */
+#define GRIP_DBG_HALT          0x08U
 
 #ifndef GRIP_NOASSERT
 #define GRIP_ASSERT(message, assertion) do { if(!(assertion)) \
@@ -45,14 +52,15 @@
  */
 #define GRIP_DEBUGF(debug, message) do { \
                                if ( \
-                                   ((s16_t)((debug) >= GRIP_DBG_LEVEL))) { \
-                                  SYS_DBG_DISPLAY_FUNC(message); \
+                                   ((debug) & GRIP_DBG_ON) && \
+                                   ((debug) & GRIP_DBG_TYPES_ON) && \
+                                   ((s16_t)((debug) & GRIP_DBG_MASK_LEVEL) >= GRIP_DBG_MIN_LEVEL)) { \
+                                 GRIP_PLATFORM_DIAG(message); \
                                  if ((debug) & GRIP_DBG_HALT) { \
                                    while(1); \
                                  } \
                                } \
                              } while(0)
-
 #else  /* GRIP_DEBUG */
 #define GRIP_DEBUGF(debug, message)
 #endif /* GRIP_DEBUG */
